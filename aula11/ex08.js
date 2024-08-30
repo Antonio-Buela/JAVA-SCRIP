0ebcc96d485d3292b8d15c4f8ff7efc4dd6c0588
Antonio-Buela <antoniomavungomano@gmail.com>
2024-08-31 00:44:43 +0100

    ex 11 javascript

---
+++ aula11/ex08.js
@@ -0,0 +1,5 @@
+var vel= 60.5
+console.log('a sua velocidade é de'+val 'km/h')
+if(vel > 60.5){
+    console.log('voce ultrapassou a velocidade permitida')
+}
---
+++ aula11/ex09.js
@@ -0,0 +1,8 @@
+var pais = 'eua'
+console.log('viveendo em Angola')
+if(pais != 'angola'){
+    console.log('voce é estrangeiro')
+}
+else{
+    console.log('voce é angolano')
+}
