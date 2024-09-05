6d8130b050eb0073b0f7c261f1b09d64deccbbad
Antonio-Buela <antoniomavungomano@gmail.com>
2024-09-06 00:51:19 +0100

    terminado modulo e

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,4 +1,22 @@
-function contar(){
+function tabuada(){
     var num = document.getElementById('contar')
-    while(num * 1 && num )
+   var tab= document.getElementById('seltab')
+
+   if(num.value.length == 0){
+    alert('por favor digite um numero')
+   
+   }
+   else{
+    var n = Number(num.value)
+    var c = 1
+    tab.innerHTML= ''
+    while( c <= 12){
+        var item = document.createElement('option')
+        item.text = n 
+        item.text +=  'x' +c
+        item.text += '=' +n*c
+        tab.appendChild(item)
+        c++
+    }
+   }
 }
