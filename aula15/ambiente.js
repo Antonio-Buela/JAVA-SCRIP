35c6af9be14ce80c9219ad605a3fefef4dbf2662
Antonio-Buela <antoniomavungomano@gmail.com>
2024-09-07 00:22:35 +0100

    vetores

---
+++ aula15/ambiente.js
@@ -0,0 +1,7 @@
+var num = [4,6,2,9]
+
+console.log('o vetor tem como posiçoes '  +num.length)
+console.log('o primeiro valor do vetor é ' +num[0])
+
+var pos = num.indexOf(2)
+console.log(pos)
---
+++ aula15/vetor.js
@@ -0,0 +1,8 @@
+var valores = [8,3,6,1,9]
+
+for(var pos=0 ; pos < valores.length ; pos++){
+    console.log(pos)
+}
+for(var pos in valores){
+    console.log(pos)
+}
