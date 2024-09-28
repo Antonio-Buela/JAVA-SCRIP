08a5d6c8f691ccfbfe9777c3a223057114f8f337
Antonio-Buela <176717718+Antonio-Buela@users.noreply.github.com>
2024-09-28 22:51:43 +0100

    terminando o curso

--- aula16ex/ex18/script.js
+++ aula16ex/ex18/script.js
@@ -3,6 +3,7 @@ var lista = document.getElementById('flista')
 var res = document.getElementById('res')
 var valores = []
 
+
 function nume(n){
     if(Number(n) >= 1 && Number(n) <= 100){
         return true
@@ -10,7 +11,7 @@ function nume(n){
         return false
     }
 }
-function list (n, 1){
+function inlista (n, 1){
     if(1.indexOf(Number(n)) != -1){
         return true
     }
@@ -18,10 +19,21 @@ function list (n, 1){
         return false
     }
 }
-function adicionar(){
-    if(nume(num.value) && !list(num.value)){
-
+    function adicionar(){
+    if(nume(num.value) && !inlista(num.value)){
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = 'valor adicionado' num.value
+        lista.appendChild(item)
     } else {
         alert('valor inválido ou já encontrado na linha ')
+    }
+    num.value= ''
+    num.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('adicione valores antes de finalizar')
     }
 }
