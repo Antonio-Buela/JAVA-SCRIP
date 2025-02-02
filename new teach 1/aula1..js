d07b5d2ddf091d9e37c7848ce5017700a599e126
Antonio-Buela <176717718+Antonio-Buela@users.noreply.github.com>
2025-02-02 22:12:47 +0000

    bbb

---
+++ new teach 1/aula1..js
@@ -0,0 +1,11 @@
+const x = 10;
+const y = 2.5;
+
+3 % 4 = //isto foca no resto da divisao
+
+const //uma constante
+let //cria variavel
+let z = 1;
+z += 1;
+z++; //isto é a variavl recebe o seu proprio valor mais o numero adicionado
+
