aee24c0ee55dd9cada9283c74a7c9b6dcb79e5bf
Antonio-Buela <176717718+Antonio-Buela@users.noreply.github.com>
2025-03-31 14:58:51 +0100

    Update aula1..js

--- new teach 1/aula1..js
+++ new teach 1/aula1..js
@@ -1,11 +1,62 @@
+
+/*
+
 const x = 10;
 const y = 2.5;
 
-3 % 4 = //isto foca no resto da divisao
+3 % 4 = 0 //isto foca no resto da divisao
 
 const //uma constante
 let //cria variavel
 let z = 1;
+
 z += 1;
 z++; //isto é a variavl recebe o seu proprio valor mais o numero adicionado
+
+z =+ 2// a variavel inicial recebe o seu proprio valor e mais 2
+
+== //igual
+!= //diferente
+&& // caso as duas forem verdadeiras(e)
+|| //caso uma dela for verdadeira(ou)
+! // inverte o valor
+
+String
+
+"pedro"
+'pedro' 
+
+//concatenação + e a variavel
++var
+//template de string
+${nome}
+//numeros de caracteres
+length
+
+//letras maisculas
+toUpperCase
+
+//letras minusculas
+toLowerCase
+
+//selecionar uma parte da string
+sustring
+console.log(s.substring(0,5))
+
+//dividir
+split
+
+// 
+
+//vetores
+["A", 10 , ""]
+
+//alertar algo no site
+window.alert('minha primeira')
+//dando alguma confirmaçao 
+window.confirm('')
+//fazendo um pergunta 
+window.prompt('')
+
+/*
 
